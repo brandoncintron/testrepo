2482d75dd7092ee03c649cd3772c235755ff1ee2
brandoncintron <bcintron19fba@gmail.com>
2025-06-29 18:46:34 -0400

    Add hello world endpoint to API

--- api/index.py
+++ api/index.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/api")
+async def helloworld():
+    return {"message": "helloworld"}
+
 @app.get("/api/health")
 async def health():
     return {"status": "ok"}
